93beb2769fed5beb230c4856a35d5651f65cb31d
Detlef Gerhardt <feedback@dgerhardt.de>
2026-08-28 20:29:46 +0200

    Update LIBDG (RANDOM.C)

---
+++ MI-C/LIBDG/RANDOM.C
@@ -0,0 +1,62 @@
+/* random.c pseudo random number generator */
+/* erzeugte mit OpenAI Codex */
+
+/* static makes it local */
+static long rndsta;
+
+/****************************************************************************/
+/*
+ * 16-bit PRNG:
+ * Multiplicative congruential generator modulo 32749.
+ *
+ * state = (23 * state) mod 32749
+ *
+ * 32749 is prime and 23 is a primitive root modulo 32749, so all seeds
+ * from 1 to 32748 produce a cycle with period 32748.
+ */
+
+int rndsed(seed)
+	int seed;
+{
+    seed %= 32749;
+    if (seed <= 0)
+        seed += 32748;
+	rndsta = seed;
+    return rndsta;
+}
+
+/****************************************************************************/
+/* get Z80 refresh counter, can be used as seed */
+
+int rndrfh()
+{
+#ASM
+	LD A,R
+	LD L,A
+	LD H,0
+#ENDASM	
+}	
+
+/****************************************************************************/
+
+#define RND_A 23
+#define RND_M 32749
+#define RND_Q 1423  /* m / a */
+#define RND_R 20 /* m % a */
+
+/* get next random number from "from" to "to-1" */
+int rndnxt(from, to)
+	int from, to;
+{
+	int hi, lo, t;
+	
+    hi = rndsta / RND_Q;
+    lo = rndsta % RND_Q;
+    rndsta = RND_A * lo - RND_Q * hi;
+    if (rndsta <= 0)
+        rndsta += RND_M;
+
+	return rndsta * (to - from) / RND_M + from;
+}
+
+/****************************************************************************/
--- MI-C/LIBDG/libdg.bat
+++ MI-C/LIBDG/libdg.bat
@@ -1,16 +1,22 @@
 REM build LIBDG.REL
 
 REM compile c modules
-CPM CCZ /SX CONIO
-CPM M80 =CONIO/Z/L
-CPM CCZ /SX CGETS
-CPM M80 =CGETS/Z/L
+CPM CCZ /SX CONIO > conio.cerr
+CPM M80 =CONIO/Z/L > conio.merr
+CPM CCZ /SX CGETS > fgets.cerr
+CPM M80 =CGETS/Z/L > fgets.merr
+CPM CCZ /SX RANDOM > random.cerr
+CPM M80 =RANDOM/Z/L > random.merr
 
 REM assemble assembler modules
-CPM M80 =MEMSET/Z/L
-CPM M80 =MEMCPY/Z/L
-CPM M80 =OUTP/Z/L
-CPM M80 =INP/Z/L
+CPM M80 =MEMSET/Z/L > memset.merr
+CPM M80 =MEMCPY/Z/L > memcpy.merr
+CPM M80 =OUTP/Z/L > outp.merr
+CPM M80 =INP/Z/L > inp.merr
 
 REM build library from modules
-CPM LIB80 LIBDG=MEMSET,MEMCPY,OUTP,INP,CONIO,CGETS/E
+CPM LIB80 LIBDG=MEMSET,MEMCPY,OUTP,INP,CONIO,CGETS,RANDOM/E > libdg.lerr
+
+MICERR conio.cerr conio.merr fgets.cerr fgets.merr random.cerr random.merr memset.merr memcpy.merr outp.merr inp.merr libdg.lerr
+
+COPY LIBDG.REL ..
